bac08a00fd319c7958ff06e8c55ce3b9f2ee0e19
RihardXXX <yusupa.akaeva@yandex.ru>
2020-01-23 19:59:19 +0300

    refactoring View modul and delete class

--- blog/url.py
+++ blog/url.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tag/<slug:slug>/', views.CategoryView.as_view(), name='tag'), # выкладывание постов по тегам
+    path('tag/<slug:slug>/', views.PostListView.as_view(), name='tag'), # выкладывание постов по тегам
     path('<slug:category>/<slug:slug>/', views.PostDetailView.as_view(), name="detail_post"), # детальный вывод статьи
-    path('<slug:category_name>/', views.CategoryView.as_view(), name="category"), # вывод статей по категориям
-    path('', views.HomeVew.as_view(), name="home")# домашняя страница и вывод всех статей
+    path('<slug:category_name>/', views.PostListView.as_view(), name="category"), # вывод статей по категориям
+    path('', views.PostListView.as_view(), name="home")# домашняя страница и вывод всех статей
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,48 +1,15 @@
 from datetime import datetime
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
 from  .models import *
 
 """тут описываем логику работы программы при получении запроса"""
 
-class HomeVew(View):
-    """Наследуемся от класса view"""
 
-    def get(self, request):
-        """Описываем что будет делать метод get"""
-        category_list = Category.objects.all()
-        post_list = Post.objects.filter(published_date__lte=datetime.now(), published=True)
-        return render(request, 'blog/post_list.html', {
-            "posts": post_list,
-            "categories": category_list
-        })
-
-    def post(self, request):
-        """описываем что будет делать метод post"""
-        pass
-
-
-class PostDetailView(View):
-    """Класс открывающий всю статью"""
-
-    def get(self, request, category, slug):
-        """Описываем что будет делать метод get"""
-        category_list = Category.objects.all()
-        post = Post.objects.get(slug=slug)
-        # tags = post.get_tags()
-        # print(tags)
-        comment = Comment.objects.filter(post_id=post.id)
-        return render(request, post.template, {
-            "post": post,
-            "categories": category_list,
-            "comments": comment
-        })
-
-
-class CategoryView(View):
-    """Вывод статей по категориям"""
+class PostListView(View):
+    """Вывод статей по категориям, тэгам, домашняя страница"""
 
     def get_queryset(self):
         return Post.objects.filter(published_date__lte=datetime.now(), published=True)
@@ -51,12 +18,28 @@ class CategoryView(View):
         """Очень умный орм запрос, получаемый объект куери сет объект постов мы ставим фильтр,
         чтобы слаг категории внутри объекта поста совпадал со слагом с маршрута юрл"""
         """Два подчеркивания это обращение"""
-        posts = []
-        category_list = Category.objects.all()
-        template = 'blog/post_list.html'
-        if category_name is not None:
+        category_list = Category.objects.all() # полный список категорий
+        template = 'blog/post_list.html' # шаблон по умолчанию
+        if category_name is not None: # если в запрос пришло название категории
             posts = self.get_queryset().filter(category__slug=category_name, category__published=True)# сортировка по категориям
-        elif slug is not None:
-            posts = self.get_queryset().filter(tags__slug=slug, tags__published=True)  # сортировка по тегам
-        # if posts.exists():
+        elif slug is not None: # если в запрос пришло название тэга
+            posts = self.get_queryset().filter(tags__slug=slug)  # сортировка по тегам
+        else: # если ничего не пришло
+            posts = self.get_queryset()  # полный список статей
         return render(request, template, {"posts": posts, "categories": category_list})
+
+
+
+class PostDetailView(View):
+    """Класс открывающий всю статью"""
+
+    def get(self, request, **kwargs):
+        """Описываем что будет делать метод get"""
+        category_list = Category.objects.all()
+        post = get_object_or_404(Post, slug=kwargs.get("slug")) # вернуть 404 если страницы не совпадают
+        comment = Comment.objects.filter(post_id=post.id)
+        return render(request, post.template, {
+            "post": post,
+            "categories": category_list,
+            "comments": comment
+        })
