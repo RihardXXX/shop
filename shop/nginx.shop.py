769438c28180cc6f7e016877c62869eb8d05c4d9
RihardXXX <yusupa.akaeva@yandex.ru>
2020-02-18 01:16:22 +0300

    training deploy server1

--- shop/nginx.shop.py
+++ shop/nginx.shop.py
@@ -1,7 +1,7 @@
 [program:course]
-command=/home/miki/venv/bin/gunicorn config.wsgi:application -c /home/miki/CourseDjango2/config/gunicorn.conf.py
-directory=/home/miki/CourseDjango2
-user=miki
+command=/home/a0212843/venv/bin/gunicorn config.wsgi:application -c /home/a0212843/CourseDjango2/config/gunicorn.conf.py
+directory=/home/a0212843/CourseDjango2
+user=a0212843
 autorestart=true
 redirect_stderr=true
-stdout_logfile = /home/miki/CourseDjango2/logs/debug.log
+stdout_logfile = /home/a0212843/CourseDjango2/logs/debug.log
